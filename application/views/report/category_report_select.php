845aa4e1910e1a2af23c7704d989a741104b0732
Naiaos84 <naiaos84@gmail.com>
2020-07-26 12:02:24 +0700

    update

---
+++ application/views/report/category_report_select.php
@@ -0,0 +1,7 @@
+
+<div class="container col-12" style="margin-top:30px">
+    เลือกปีที่ต้องการ<br>
+    2561<br>
+    <a href="<?php echo site_url('/report/CategoryReport62');?>">2562</a><br>
+    <a href="<?php echo site_url('/report/CategoryReport63');?>">2563</a><br>
+</div>
