c0023947cd729630c11161e5635c8d2a239f659a
Naiaos84 <naiaos84@gmail.com>
2020-09-20 10:44:05 +0700

    Update chk.php

--- chk.php
+++ chk.php
@@ -5,7 +5,13 @@ $password ="";
 $dbname ="umcity_umstock";	
 
 $conn = new mysqli($servername, $username, $password, $dbname);
+echo "<form action='chk.php'><input type='text'name='id'></form>";
 
+if(empty($id)){
+    echo "show";
+}else{
+    echo "not show";
+}
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
@@ -21,9 +27,9 @@ $result = $conn->query($sql1);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<pre>";
+        // echo "<pre>";
         print_r($row);
-        echo "</pre>";
+        // echo "</pre>";
         $orderNum= $row['orderNum'];
         $orderID= $row['orderID'];
     }
@@ -31,34 +37,37 @@ if ($result->num_rows > 0) {
     echo "0 results";
 }
 
+echo "<hr>";
 
 $sql2="SELECT * from tb_orderdetail where orderID=$orderNum";
 $result2 = $conn->query($sql2);
-echo "<br>";
+// echo "<br>";
 
 if ($result2->num_rows > 0) {
     // output data of each row
     while($row2 = $result2->fetch_assoc()) {
-        echo "<pre>";
+        // echo "<pre>";
         print_r($row2);
-        echo "</pre>";
+        // echo "</pre>";
         // $orderID= $row['orderID'];
+        echo "<br>";
     }
 } else {
     echo "0 results";
 }
-
+echo "<hr>";
 $sql3="SELECT * from tb_orderfile where orderID=$orderID";
 $result3 = $conn->query($sql3);
-echo "<br>";
+// echo "<br>";
 
 if ($result3->num_rows > 0) {
     // output data of each row
     while($row3 = $result3->fetch_assoc()) {
-        echo "<pre>";
+        // echo "<pre>";
         print_r($row3);
-        echo "</pre>";
+        // echo "</pre>";
         // $orderID= $row['orderID'];
+        echo "<br>";
     }
 } else {
     echo "0 results";
